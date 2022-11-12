0fb42e9e69896a992808930b936682d8aca0871f
shadid12 <shadidhaque2012@gmail.com>
2022-11-11 17:40:45 -0800

    exp edge

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,6 +2,14 @@ export const config = {
   runtime: 'experimental-edge',
 }
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function (req) {
+  return new Response(
+    JSON.stringify({ name: 'John Doe' }),
+    {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+  )
 }
