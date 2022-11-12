a2f41fefc84c6aaf0ddf4d23c0fc263b7efbec30
shadid12 <shadidhaque2012@gmail.com>
2022-11-12 07:38:17 -0500

    deploy all

--- db.js
+++ db.js
@@ -13,3 +13,15 @@ export const createProduct = async (title, price, description) => {
   };
   return client.query(q.Create(q.Collection('Products'), product));
 };
+
+
+export const getProducts = async () => {
+  const response = await client.query(
+    q.Map(
+      q.Paginate(q.Documents(q.Collection('Products'))),
+      q.Lambda(x => q.Get(x))
+    )
+  );
+  const products = response.data.map((product) => ({ ...product.data, _id: product.ref.id }));
+  return products;
+}
